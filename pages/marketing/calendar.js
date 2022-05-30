084384904f6ca86e76fe833ff8fc4ab0d3824547
joko19 <jokoslamet459@gmail.com>
2022-05-30 16:09:58 +0700

    your note

--- pages/marketing/calendar.js
+++ pages/marketing/calendar.js
@@ -12,6 +12,7 @@ import {
     MonthView
 } from '@devexpress/dx-react-scheduler-material-ui';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
+import { MdOutlineEventNote } from 'react-icons/md';
 
 const currentDate = '2022-05-22';
 const schedulerData = [
@@ -29,18 +30,40 @@ export default function MarketingDashboard() {
             {/* Marketing Calendar */}
             <section className='px-4'>
                 <h1 className='text-red-500 font-bold text-3xl mb-4'>Marketing Calendar</h1>
-                <Scheduler
-                    data={schedulerData}
-                >
-                    <ViewState
-                        currentDate={currentDate}
-                    />
-                    <EditingState onCommitChanges={saveAppointment}/>
-                    <IntegratedEditing/>
-                    <MonthView />
-                    <Appointments />
-                    <AppointmentForm/>
-                </Scheduler>
+                <div className='flex gap-4'>
+                    <Card className='w-full'>
+                        <Scheduler
+                            data={schedulerData}
+                        >
+                            <ViewState
+                                currentDate={currentDate}
+                            />
+                            <EditingState onCommitChanges={saveAppointment} />
+                            <IntegratedEditing />
+                            <MonthView />
+                            <Appointments />
+                            <AppointmentForm />
+                        </Scheduler>
+                    </Card>
+                    <Card className='w-1/3'>
+                        <div className='flex gap-2'>
+                            <MdOutlineEventNote color='white' className='bg-red-500 rounded my-auto' size={16} />
+                            <h1 className='font-bold text-xl '>Your Note</h1>
+                        </div>
+                        <div className='border-b flex mt-4 text-gray-700'>
+                            <span className='w-full'>Date</span><span className='w-full'>Note</span>
+                        </div>
+                        <div className='flex mt-4 text-gray-700'>
+                            <span className='w-full text-red-500'>19 Mei 2022</span><span className='w-full'>Lorem Ipsum</span>
+                        </div>
+                        <div className='flex mt-4 text-gray-700'>
+                            <span className='w-full text-red-500'>25 Mei 2022</span><span className='w-full'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </span>
+                        </div>
+                        <div className='flex mt-4 text-gray-700'>
+                            <span className='w-full text-red-500'>30 Mei 2022</span><span className='w-full'>Lorem Ipsum is simply dummy text</span>
+                        </div>
+                    </Card>
+                </div>
             </section>
         </Layout>
     )
